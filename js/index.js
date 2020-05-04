66d40b4f0f71b765a11e1466efaff2633f48929d
Nathan Cowley <nathancowley12@gmail.com>
2020-05-04 15:23:52 -0600

    did a different way of layout than before. Made it more organized

--- js/index.js
+++ js/index.js
@@ -38,127 +38,82 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-/*NAVIGATION*/
+/****************NAVIGATION***************/
 
- const navItems = document.querySelectorAll('a');
- navItems[0].textContent = siteContent['nav']['nav-item-1'];
- navItems[1].textContent = siteContent['nav']['nav-item-2'];
- navItems[2].textContent = siteContent['nav']['nav-item-3'];
- navItems[3].textContent = siteContent['nav']['nav-item-4'];
- navItems[4].textContent = siteContent['nav']['nav-item-5'];
- navItems[5].textContent = siteContent['nav']['nav-item-6'];
+let nav1 = document.querySelectorAll('nav a')
+nav1.forEach((value, index) => {
+  value.textContent = siteContent.nav[`nav-item-${index + 1}`];
+});
 
- navItems[0].style.color = 'green';
- navItems[1].style.color = 'green';
- navItems[2].style.color = 'green';
- navItems[3].style.color = 'green';
- navItems[4].style.color = 'green';
- navItems[5].style.color = 'green';
+nav1.forEach((element) => {
+  element.style.color = 'green'
+});
 
- const navBar = document.querySelector('nav');
- const addNav1 = document.createElement('a');
- const addNav2 = document.createElement('a');
+const newLink = document.createElement('a')
+newLink.innerHTML = 'MyLink';
+document.querySelector('nav').appendChild(newLink)
+newLink.style.color = 'green'
 
- addNav1.href = '#';
- addNav1.textContent = 'Home';
- addNav1.style.color = 'green';
+const newLink2 = document.createElement('a')
+newLink2.innerHTML = 'DOM',
+document.querySelector('nav').appendChild(newLink2)
+newLink2.style.color = 'green'
 
- addNav2.href = '#';
- addNav2.textContent = 'Nate';
- addNav2.style.color = 'green';
-
- navBar.prepend(addNav1);
- navBar.append(addNav2);
+const logoImg = document.getElementById('logo-img');
+logoImg.setAttribute('src', siteContent['nav']['img-src'])  
 
 
 
- /*CTA*/
 
- const ctaText = document.querySelector('h1')
+/**********CTA**********/
 
- ctaText.textContent = 'DOM IS Awesome'
- ctaText.style.color = 'black';
+const title = document.querySelector('h1')
+title.textContent = siteContent['cta']['h1']
 
- const ctaButton = document.querySelector('button')
- ctaButton.textContent = 'Get started';
- ctaButton.style.color = 'grey';
+const button = document.querySelector('button')
+button.textContent = siteContent['cta']['button']
 
- document.getElementById('cta-img').src = 'img/header-img.png';
+const ctaImg = document.getElementById('cta-img')
+ctaImg.setAttribute('src', siteContent['cta']['img-src'])
 
 
- /*FEATURES*/
 
- const featuresH4 = document.getElementsByTagName('h4')[0]
+/***********MAIN-CONTENT**********/
 
- featuresH4.textContent = 'Features';
- featuresH4.style.color = 'black';
+const h4s = document.querySelectorAll('h4');
+h4s[0].textContent = siteContent['main-content']['features-h4'];
+h4s[1].textContent = siteContent['main-content']['about-h4'];
+h4s[2].textContent = siteContent['main-content']['services-h4'];
+h4s[3].textContent = siteContent['main-content']['product-h4'];
+h4s[4].textContent = siteContent['main-content']['vision-h4'];
+h4s[5].textContent = siteContent['contact']['contact-h4'];
 
- const featuresP = document.getElementsByTagName('p')[0]
- featuresP.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
- featuresP.style.color = 'black';
 
- /*ABOUT*/
+const middleImg = document.getElementById('middle-img')
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
 
-const aboutH4 = document.getElementsByTagName('h4')[1]
-aboutH4.textContent = 'About';
-aboutH4.style.color = 'black';
 
-const aboutP = document.getElementsByTagName('p')[1]
-aboutP.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-aboutP.style.content = 'black';
+const mainP = document.querySelectorAll('.main-content p')
+mainP[0].textContent = siteContent['main-content']['features-content']
+mainP[1].textContent = siteContent['main-content']['about-content']
+mainP[2].textContent = siteContent['main-content']['services-content']
+mainP[3].textContent = siteContent['main-content']['product-content']
+mainP[4].textContent = siteContent['main-content']['vision-content']
 
-/*MIDDLE IMG*/
 
- document.getElementById('middle-img').src = 'img/mid-page-accent.jpg';
+/***************CONTACT*************/
 
- /*SERVICES*/
+const contactP = document.querySelectorAll('.contact p')
+contactP[0].textContent = siteContent['contact']['address']
+contactP[1].textContent = siteContent['contact']['phone']
+contactP[2].textContent = siteContent['contact']['email']
 
- const servicesH4 = document.getElementsByTagName('h4')[2]
- servicesH4.textContent = 'Services';
- servicesH4.style.color = 'black';
 
- const servicesP = document.getElementsByTagName('p')[2]
- servicesP.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
 
- servicesP.style.color = 'black';
 
- /*PRODUCT*/
 
- const productH4 = document.getElementsByTagName('h4')[3]
- productH4.textContent = 'Product';
- productH4.style.color = 'black';
 
- const productP = document.getElementsByTagName('p')[3]
- productP.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-
- productP.style.color = 'black';
-
- /*VISION*/
-
-const visionH4 = document.getElementsByTagName('h4')[4]
-visionH4.textContent = 'Vision';
-visionH4.style.color = 'black';
-
-const visionP = document.getElementsByTagName('p')[4]
-visionP.textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-
-visionP.style.color = 'black';
-
-/*CONTACT*/
-
-const contactH4 = document.querySelector(".contact h4");
-contactH4.textContent = siteContent["contact"]["contact-h4"];
-
-const contactP = document.querySelectorAll(".contact p");
-contactP[0].innerHTML = "123 Way 456 Street<br>Somewhere, USA" 
-contactP[1].textContent = siteContent["contact"]["phone"];
-contactP[2].textContent = siteContent["contact"]["email"];
-
-/*FOOTER*/
-
-const copyright = document.querySelector("footer p");
-copyright.textContent = siteContent["footer"]["copyright"];
 
